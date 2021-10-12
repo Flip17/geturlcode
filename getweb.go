2734465f297924311be31f59baedef9c01beebf4
Flip17 <78990164+Flip17@users.noreply.github.com>
2021-10-13 01:44:28 +0200

    Add files via upload

--- getweb.go
+++ getweb.go
@@ -1,17 +1,14 @@
 package main
+
 // +ignore build
 import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"log"
 	"html/template"
-
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
-
-
-func renderTemp ( w http.ResponseWriter, r *http.Request) {
+func renderTemp(w http.ResponseWriter, r *http.Request) {
 	parsedTemplate, _ := template.ParseFiles("form.html")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
@@ -19,32 +16,21 @@ func renderTemp ( w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
-	
-func geturl(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	fmt.Fprint(w, "URL : ", r.FormValue("geturl"))
-	
 
-}
-
-func showurl (w http.ResponseWriter, r *http.Request) {
+func showurl(w http.ResponseWriter, r *http.Request) {
 	var linkurl = r.FormValue("geturl")
 	resp, err := http.Get(linkurl)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	
+
 	sb := string(body)
-	
+
 	t, err := template.ParseFiles("form.html")
 	links := struct {
 		Url string
@@ -52,16 +38,16 @@ func showurl (w http.ResponseWriter, r *http.Request) {
 		Url: sb,
 	}
 	t.Execute(w, links)
-	
+
 }
 
-
-	
 func main() {
-	
+
 	http.HandleFunc("/", renderTemp)
 	http.HandleFunc("/geturl", showurl)
-		
-	
-	
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
 }
